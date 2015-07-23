59ced0b92fd16f196697c2771126bb1ada19f79d
Fabio Crestoni <fabio.crestoni@gmail.com>
2015-07-23 15:34:28 +0200

    Update check-oas_poslist-vs-id.js
    Aggiunto UA iPad

--- check-oas_poslist-vs-id.js
+++ check-oas_poslist-vs-id.js
@@ -18,26 +18,41 @@ var checkoas = function(url) {
     if (UA == 'iPhone') {
         page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
     }
+    if (UA == 'iPad') {
+        page.settings.userAgent = 'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
+    }
 
     page.onConsoleMessage = function(msg) {
         console.log(msg);
     };
 
+
+    page.onError = function(msg, trace) {};
+
     page.open(url, function() {
-        console.log('__________________ \n ');
-        console.log('URL: ', url);
+       console.log('_____________________________________________________________ \n ');
+       console.log('URL: ', url);
         page.evaluate(function() {
            console.log('Lista posizioni OAS: ', poslist);
             var poslist_array = poslist.split(',');
 
             for (i = 0; i < poslist_array.length; i++) {
                 var id = document.getElementById(poslist_array[i]);
+                
+                //dots
+                var dots;
+                var add = 20-poslist_array[i].length;
+                var dots = '';
+                for (var x = 0 ; x < add; x++) {
+                    dots +='.';
+                }
+
                 if (id) {
-                    id = poslist_array[i] + ' OK';
+                    id = poslist_array[i] +' '+dots+ ' OK';
                 } else {
-                    id = poslist_array[i] + ' KO - Warning';
+                    id = poslist_array[i] +' '+dots+ ' KO - Warning';
                 } 
-                console.log (id);
+                console.log(id);
             };
         });
 
@@ -53,6 +68,11 @@ var checkoas = function(url) {
 
     });
 
+
+
+
 }
+
+
 
 checkoas(listOfUrls.pop());
